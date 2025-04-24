36ecb61f92537d942a1e9391beac76f357087172
Yuri Kuit <yuri.kuit+github@gmail.com>
2025-04-24 20:04:17 +0200

    first push

---
+++ _up.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#set -e
+##################################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################################
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - ArcoLinux alias - grh
+
+# reset - go back one commit - all is lost
+# git reset --hard HEAD~1
+
+# remove a file online but keep it locally
+# https://www.baeldung.com/ops/git-remove-file-without-deleting-it
+# git rm --cached file.txt
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin main
+
+echo
+tput setaf 6
+echo "##############################################################"
+echo "###################  $(basename $0) done"
+echo "##############################################################"
+tput sgr0
+echo
